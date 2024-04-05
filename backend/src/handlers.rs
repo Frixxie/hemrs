c15cb73991833a92d0696e06bcd2e1c202f5131a
fredrik <freddahf@outlook.com>
2024-04-05 19:34:12 +0200

    wip

--- backend/src/handlers.rs
+++ backend/src/handlers.rs
@@ -7,11 +7,35 @@ use sensors::Dht11;
 use crate::create::Create;
 use crate::db_connection_pool::Postgres;
 
+use crate::devices::Device;
 use crate::error::HandlerError;
 use crate::measurements::Measurement;
 use crate::read::Read;
+use crate::sensors::Sensor;
 
-pub async fn store_env_data(
+pub async fn fetch_devices(
+    State(pg_pool): State<Postgres>,
+) -> Result<Json<Vec<Device>>, HandlerError> {
+    info!("GET api/devices");
+    let devices = Vec::<Device>::read(pg_pool).await.map_err(|e| {
+        warn!("Failed with error: {}", e);
+        HandlerError::new(500, format!("Failed to fetch data from database: {}", e))
+    })?;
+    Ok(Json(devices))
+}
+
+pub async fn fetch_sensors(
+    State(pg_pool): State<Postgres>,
+) -> Result<Json<Vec<Sensor>>, HandlerError> {
+    info!("GET api/devices");
+    let sensors = Vec::<Sensor>::read(pg_pool).await.map_err(|e| {
+        warn!("Failed with error: {}", e);
+        HandlerError::new(500, format!("Failed to fetch data from database: {}", e))
+    })?;
+    Ok(Json(sensors))
+}
+
+pub async fn store_measurements(
     State(pg_pool): State<Postgres>,
     Json(dht11_data): Json<Dht11>,
 ) -> Result<String, HandlerError> {
@@ -25,10 +49,10 @@ pub async fn store_env_data(
     Ok("OK".to_string())
 }
 
-pub async fn fetch_latest_data(
+pub async fn fetch_latest_measurement(
     State(pg_pool): State<Postgres>,
 ) -> Result<Json<Measurement>, HandlerError> {
-    info!("GET api/latest");
+    info!("GET api/measurements/latest");
     let entry = Measurement::read(pg_pool).await.map_err(|e| {
         warn!("Failed with error: {}", e);
         HandlerError::new(500, format!("Failed to fetch data from database: {}", e))
@@ -37,10 +61,10 @@ pub async fn fetch_latest_data(
     Ok(Json(entry))
 }
 
-pub async fn fetch_all_data(
+pub async fn fetch_all_measurements(
     State(pool): State<Postgres>,
 ) -> Result<Json<Vec<Measurement>>, HandlerError> {
-    info!("GET api/all");
+    info!("GET api/measurements/all");
     let entry = Vec::<Measurement>::read(pool).await.map_err(|e| {
         warn!("Failed with error: {}", e);
         HandlerError::new(500, format!("Failed to fetch data from database: {}", e))
